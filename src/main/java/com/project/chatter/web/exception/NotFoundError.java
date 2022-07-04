0568b0b1b624e483ccab5141ead0c9946d4e1ef7
Simeon Simeonov <simeonsimeonov006@gmail.com>
2022-07-04 09:59:19 +0300

    Added a new constructor of the not found error with a generic message

--- src/main/java/com/project/chatter/web/exception/NotFoundError.java
+++ src/main/java/com/project/chatter/web/exception/NotFoundError.java
@@ -6,4 +6,8 @@ public class NotFoundError extends RuntimeException {
     public NotFoundError(String message) {
         super(message);
     }
+
+    public NotFoundError() {
+        super("Object not found");
+    }
 }
